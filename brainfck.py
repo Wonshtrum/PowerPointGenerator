99cf3d8aa14524ddcc8550d6e562606ce0847e7a
Wonshtrum <pygmee64@gmail.com>
2021-10-31 21:42:32 +0100

    Little experiment with a brainfck interpreter

---
+++ brainfck.py
@@ -0,0 +1,135 @@
+from pptgen import *
+
+
+def iterate(*elements):
+	for element in elements:
+		try:
+			for sub_element in element:
+				yield sub_element
+		except TypeError:
+			yield element
+
+
+class Bit:
+	def __init__(self, x, y, w, first=False):
+		self.zero = Shape(x, y, w, w, text=0)
+		self.one  = Shape(x, y, w, w, text=1)
+		tl.add(Appear(self.one), on=self.zero)
+		tl.add(Disappear(self.zero), on=self.zero)
+		tl.add(Place(self.zero), on=self.zero)
+		tl.add(Place(self.one), on=self.zero)
+		tl.add(Appear(self.zero), on=self.one)
+		tl.add(Disappear(self.one), on=self.one)
+		tl.add(Place(self.zero), on=self.one)
+		tl.add(Place(self.one), on=self.one)
+		if first:
+			self.next0 = self.next1 = None
+		else:
+			self.next0 = Shape(x, y, w, w, text="0-", z=1)
+			self.next1 = Shape(x, y, w, w, text="1+", z=1)
+			tl.add(Target(self.next1), on=self.one)
+			tl.add(Target(self.next0), on=self.zero)
+			tl.add(Place(self.next1), on=self.next1)
+			tl.add(Place(self.next0), on=self.next0)
+
+	def link(self, other):
+		tl.add(Target(other.zero), on=self.next0)
+		tl.add(Target(other.one), on=self.next0)
+		tl.add(Target(other.zero), on=self.next1)
+		tl.add(Target(other.one), on=self.next1)
+
+
+class Word:
+	DEFAULT_N_BITS = 8
+	def __init__(self, x, y, w, h=True):
+		v = not h
+		self.bits = []
+		for i in range(Word.DEFAULT_N_BITS):
+			bit = Bit(x+w*i*h, y+w*i*v, w)
+			if i > 0:
+				bit.link(self.bits[-1])
+			self.bits.append(bit)
+
+		self.inc = Shape(x+(i+1)*w*h, y+(i+1)*w*v, w, w, (0, 255, 0), text="I", z=-1)
+		self.dec = Shape(x+(i+2)*w*h, y+(i+2)*w*v, w, w, (255, 0, 0), text="D", z=-1)
+		self.reset = Shape(x+(i+2)*w*h, y+(i+2)*w*v, w, w, (120, 120, 120), text="R", z=-1)
+		self.left = Shape(x+(i+3)*w*h, y+(i+3)*w*v, w, w, (0, 0, 255), text="⯇", z=-2)
+		self.right = Shape(x+(i+3)*w*h, y+(i+3)*w*v, w, w, (0, 0, 255), text="⯈", z=-2)
+		tl.add(Place(self.reset), on=self.reset)
+		for control in self.get_controls():
+			tl.add(Place(control), on=self.left)
+			tl.add(Place(control), on=self.right)
+			tl.add(Disappear(control), on=control)
+
+		for bit in self.bits:
+			tl.add(Place(bit.next0), on=self.reset)
+			tl.add(Place(bit.next1), on=self.reset)
+			tl.add(Appear(bit.next1), on=self.inc)
+			tl.add(Appear(bit.next0), on=self.dec)
+			tl.add(Disappear(bit.next0), on=self.inc)
+			tl.add(Disappear(bit.next1), on=self.dec)
+
+		lsb = self.bits[-1]
+		tl.add(Target(lsb.one), on=self.inc)
+		tl.add(Target(lsb.one), on=self.dec)
+		tl.add(Target(lsb.zero), on=self.inc)
+		tl.add(Target(lsb.zero), on=self.dec)
+		tl.add(Target(self.reset), on=self.inc)
+		tl.add(Target(self.reset), on=self.dec)
+
+	def get_controls(self):
+		return [self.inc, self.dec, self.left, self.right]
+
+
+class Cell:
+	SYMBOLS = "+-⯇⯈[]"
+	def __init__(self, x, y, w):
+		self.cycle = Shape(x, y+w, w, w)
+		self.symbols = [Shape(x, y, w, w, text=symbol) for i, symbol in enumerate(Cell.SYMBOLS)]
+		for i, symbol in enumerate(self.symbols):
+			tl.add(Appear(symbol, click=True), on=self.cycle)
+			for other in self.symbols[:i]:
+				tl.add(Disappear(other), on=self.cycle)
+
+
+Word.DEFAULT_N_BITS = 4
+Text.DEFAULT_COLOR = (255, 255, 255)
+tl = Timeline()
+Target = lambda s: Place(s, target, relative=False)
+
+w = 3
+tx = 0
+ty = 50
+target = (tx, ty)
+
+n_cells = 10
+n_words = 10
+
+
+ox = 0
+oy = 0
+controlers = []
+for i, symbol in enumerate("+-⯇⯈"):
+	s = Shape(ox+i*w, oy, w, w, (255, 0, 0), text=symbol)
+	controlers.append(s)
+
+
+ox = 10
+oy = ty
+cells = [Cell(ox+x*w, oy, w) for x in range(n_cells)]
+
+ox = 10
+oy = 10
+words = [Word(ox+w*i, oy, w, False) for i in range(n_words)]
+for i, word in enumerate(words):
+	left = words[(i-1)%n_words]
+	right = words[(i+1)%n_words]
+	for control in word.get_controls():
+		tl.add(Target(control), on=right.left)
+		tl.add(Target(control), on=left.right)
+
+
+shapes = Shape.dump()
+slide = Slide("brainfck", shapes, tl)
+doc = Document("brainfck", [slide])
+doc.save()
