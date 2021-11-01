c66f42a1ca5e4342e1d897ec6ad37f4e50e22912
Wonshtrum <pygmee64@gmail.com>
2021-11-01 19:11:05 +0100

    Working loops

--- brainfck.py
+++ brainfck.py
@@ -16,16 +16,22 @@ def iterate(*elements):
 
 class Bit:
 	def __init__(self, x, y, w, first=False):
+		self.not_zero = Shape(x, y, w/2, w, (255, 0, 255), z=-2)
 		self.zero = Shape(x, y, w, w, text=0)
 		self.one  = Shape(x, y, w, w, text=1)
+
+		tl.add(Appear(self.not_zero), on=self.zero)
 		tl.add(Appear(self.one), on=self.zero)
 		tl.add(Disappear(self.zero), on=self.zero)
 		tl.add(Place(self.zero), on=self.zero)
 		tl.add(Place(self.one), on=self.zero)
+
+		tl.add(Disappear(self.not_zero), on=self.one)
 		tl.add(Appear(self.zero), on=self.one)
 		tl.add(Disappear(self.one), on=self.one)
 		tl.add(Place(self.zero), on=self.one)
 		tl.add(Place(self.one), on=self.one)
+
 		if first:
 			self.next0 = self.next1 = None
 		else:
@@ -59,7 +65,10 @@ class Word:
 		self.left = Shape(x+(i+3)*w*h, y+(i+3)*w*v, w, w, (0, 0, 255), text="⯇", z=-1)
 		self.right = Shape(x+(i+3)*w*h, y+(i+3)*w*v, w, w, (0, 0, 255), text="⯈", z=-1)
 		self.reset = Shape(x+(i+4)*w*h, y+(i+4)*w*v, w, w, (120, 120, 120), text="R", z=-1)
+		self.test = Shape(x+(i+5)*w*h, y+(i+5)*w*v, w, w, (255, 0, 255), text="T", z=-1)
+
 		tl.add(Place(self.reset), on=self.reset)
+
 		for control in self.get_controls():
 			tl.add(Place(control), on=self.left)
 			tl.add(Place(control), on=self.right)
@@ -71,6 +80,7 @@ class Word:
 			tl.add(Appear(bit.next0), on=self.dec)
 			tl.add(Disappear(bit.next0), on=self.inc)
 			tl.add(Disappear(bit.next1), on=self.dec)
+			tl.add(Target(bit.not_zero), on=self.test)
 
 		lsb = self.bits[-1]
 		tl.add(Target(lsb.one), on=self.inc)
@@ -81,7 +91,7 @@ class Word:
 		tl.add(Target(self.reset), on=self.dec)
 
 	def get_controls(self):
-		return [self.inc, self.dec, self.left, self.right]
+		return [self.inc, self.dec, self.left, self.right, self.test]
 
 
 class Cell:
@@ -149,23 +159,31 @@ n_stack = 3
 n_stack += 1
 
 
+start = Shape(tx, ty, w, w, (0, 255, 0), text="S", z=-4)
+ox = 10
+oy = ty
+cells = [Cell(ox+x*w, oy, w) for x in range(n_cells)]
+ox = 10
+oy = 10
+words = [Word(ox+w*i, oy, w, False) for i in range(n_words)]
+
+
 ox = 0
 oy = 0
 controler = Object()
 controler.symbols = []
 for i, (symbol, name) in enumerate(zip("+-⯇⯈[]", ("INC", "DEC", "LEFT", "RIGHT", "PUSH", "POP"))):
-	s = Shape(ox+i*w, oy, w, w, (255, 0, 0), text=symbol, z=-2)
+	s = Shape(ox+i*w, oy, w, w, (255, 0, 0), text=symbol, z=-3)
 	setattr(controler, name, s)
 	controler.symbols.append(s)
-controler.reset = Shape(ox+(i+1)*w+1, oy, w, w, (255, 0, 0), text="R", z=-2)
-controler.reset_next = Shape(ox+(i+2)*w+2, oy, w, w, (0, 0, 255), text="R", z=-2)
-tl.add(Place(controler.reset), on=controler.reset)
-tl.add(Place(controler.reset_next), on=controler.reset_next)
+controler.RESET = Shape(ox+(i+1)*w+1, oy, w, w, (255, 0, 0), text="R", z=-3)
+controler.LOOP  = Shape(ox+(i+2)*w+2, oy, w, w, (255, 0, 0), text="L", z=-3)
+tl.add(Place(controler.RESET), on=controler.RESET)
+tl.add(Place(controler.LOOP), on=controler.LOOP)
 
 
 ox = 10
 oy = ty
-cells = [Cell(ox+x*w, oy, w) for x in range(n_cells)]
 for i, cell in enumerate(cells):
 	for symbol in cell.symbols:
 		tl.add(Place(symbol), on=symbol)
@@ -177,15 +195,16 @@ for i, cell in enumerate(cells):
 	if i+1 < n_cells:
 		right = cells[i+1]
 		tl.add(Target(right.place), on=cell.place)
-		tl.add(Place(right.place), on=controler.reset_next)
-	tl.add(Target(controler.reset_next), on=cell.stack[1][1])
+		tl.add(Place(right.place), on=controler.LOOP)
+	tl.add(Appear(cell.next), on=controler.LOOP)
+	tl.add(Place(cell.next), on=controler.LOOP)
+	tl.add(Disappear(cell.next), on=controler.POP)
 for s in controler.symbols:
 	tl.add(Place(s), on=s)
 
 
 ox = 10
 oy = 10
-words = [Word(ox+w*i, oy, w, False) for i in range(n_words)]
 for i, word in enumerate(words):
 	left = words[(i-1)%n_words]
 	right = words[(i+1)%n_words]
@@ -193,11 +212,23 @@ for i, word in enumerate(words):
 	tl.add(Appear(word.dec), on=controler.DEC)
 	tl.add(Appear(word.left), on=controler.LEFT)
 	tl.add(Appear(word.right), on=controler.RIGHT)
+	tl.add(Appear(word.test), on=controler.POP)
 	for control in word.get_controls():
 		tl.add(Target(control), on=right.left)
 		tl.add(Target(control), on=left.right)
-		tl.add(Target(controler.reset), on=control)
-		tl.add(Disappear(control), on=controler.reset)
+		tl.add(Target(controler.RESET), on=control)
+		tl.add(Disappear(control), on=controler.RESET)
+
+	for bit in word.bits:
+		tl.add(Target(controler.LOOP), on=bit.not_zero)
+		tl.add(Place(bit.not_zero), on=controler.LOOP)
+		tl.add(Disappear(bit.next0), on=start)
+		tl.add(Disappear(bit.next1), on=start)
+
+
+tl.add(Disappear(start), on=start)
+tl.add(Target(words[-1].right), on=start)
+tl.add(Target(cells[0].place), on=start)
 
 
 shapes = Shape.dump()
