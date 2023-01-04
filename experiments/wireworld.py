7b936bdf6e97b457aa447abb5007333011e26f3e
Eloi DEMOLIS <eloi.demolis@clever-cloud.com>
2023-01-04 09:06:58 +0100

    Fixed Wireworld head counter reset
    Signed-off-by: Eloi DEMOLIS <eloi.demolis@clever-cloud.com>

--- experiments/wireworld.py
+++ experiments/wireworld.py
@@ -11,12 +11,13 @@ def in_bound(x, y, w, h):
     return x>=0 and y>=0 and x<w and y<h
 
 
+DEBUG = 0
 class Cell:
     def __init__(self, x, y, w):
         base = Shape(x, y, w, w, Style(None, WHITE, 0.1), z=5)
         wire = Shape(x, y, w, w, Style((255, 200, 0), WHITE, 0.1), z=4)
         tail = Shape(x, y, w, w, Style((255, 0, 0), WHITE, 0.1), z=3)
-        head = Shape(x, y, w, w, Style((0, 0, 255), WHITE, 0.1), z=1)
+        head = Shape(x, y, w, w, Style((0, 0, 255), WHITE, 0.1), z=-1)
 
         self.base = base
         self.wire = wire
@@ -24,30 +25,29 @@ class Cell:
         self.head = head
 
         mw = w/3
-        debug = 0
+        self.counter = [
+            Shape(DEBUG*(x+12*w), DEBUG*(y+(i+3)*mw), mw*3, mw, (100, 100, 100), z=0)
+            for i in range(3)
+        ]
         self.connect = [
-            Shape(debug*(x+i*mw+6*w), debug*(y+j*mw), mw, mw, (0, 255, 0), z=0)
+            Shape(DEBUG*(x+i*mw+6*w), DEBUG*(y+j*mw), mw, mw, (0, 255, 0), z=0)
             for i in range(3)
             for j in range(3)
             if i != 1 or j != 1
         ]
-        self.pre = [
-            Shape(debug*(x+12*w), debug*(y+(i+3)*mw), mw*3, mw, (100, 100, 100), z=0)
-            for i in range(3)
-        ]
         self.confirm = Shape(tx, ty, w, w, (0, 255, 255), z=2)
         self.delete = Shape(x, y, w, w, Style((255, 100, 0), WHITE, 0.1))
 
-        for (i, _) in enumerate(self.pre):
+        for (i, _) in enumerate(self.counter):
             if i < 2:
-                tl.add(Reset(self.pre[i+1]), on=_)
+                tl.add(Reset(self.counter[i+1]), on=_)
             tl.add(Disappear(_), on=_)
 
-        reset = self.pre[-1]
+        reset = self.counter[-1]
         for _ in self.connect:
             tl.add(Reset(_), on=_)
             tl.add(Reset(self.confirm), on=_)
-            for p in self.pre:
+            for p in self.counter:
                 tl.add(Target(p), on=_)
             tl.add(Reset(_), on=reset)
 
@@ -57,9 +57,9 @@ class Cell:
         tl.add(Reset(head), on=self.confirm)
 
     def on_head(self):
-        base, wire, tail, head = self.base, self.wire, self.tail, self.head
+        base, wire, tail, head, delete = self.base, self.wire, self.tail, self.head, self.delete
 
-        for _ in self.pre:
+        for _ in self.counter:
             tl.add(Disappear(_), on=head)
 
         for _ in self.connect:
@@ -68,9 +68,9 @@ class Cell:
             tl.add(Disappear(_), on=wire)
             tl.add(Disappear(_), on=self.confirm)
 
-        tl.add(Appear(self.delete), on=head)
-        tl.add(Disappear(self.delete), on=self.delete)
-        tl.add(Disappear(tail), on=self.delete)
+        tl.add(Appear(delete), on=head)
+        tl.add(Disappear(self.delete), on=delete)
+        tl.add(Disappear(tail), on=delete)
 
         tl.add(Appear(wire), on=base)
         tl.add(Appear(head), on=wire)
@@ -85,8 +85,8 @@ WHITE = (255, 255, 255)
 
 tl = Timeline()
 
-n_rows = 10
-n_cols = 25
+n_rows = 15
+n_cols = 20
 w = 2
 ox = w
 oy = w
@@ -125,6 +125,7 @@ for i in range(n_cols):
         cell = cells[j][i]
         tl.add(Target(cell.head), on=step)
         tl.add(Target(cell.tail), on=step)
+        tl.add(Reset(cell.counter[0]), on=step)
         tl.add(Place(cell.delete, (0, 0), relative=False), on=start)
         for _ in cell.connect:
             tl.add(Place(_), on=start)
